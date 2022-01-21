25067335b53865ea489aa82f8b26a72395d13740
Pravesh <praveshnayak@gmail.com>
2022-01-21 23:25:13 +0530

    initial commit: various ways to print

---
+++ 1_intro.py
@@ -0,0 +1,23 @@
+## Python3
+# Beginning of Python3
+
+# Single line comments in python is identified-
+# by a hash at the beginning of the comment
+
+# print to the console
+print('First Hello World')
+print("Second Hello World")
+print('\n\n')
+print('A', 'B', 'z')
+print(10)
+print(1, '\t', 2, '\t\t', 3)
+print('')
+print(5.55)
+print(100, 5.55, 'Third Hello World')
+print('\n')
+print(10, 20, 30, sep='#')
+print(10, 20, 30, end='#')
+print()
+print('A'+'b'+'X')
+# print('Hello ' + 5 + 'World')     #This produces error
+# print(any_Object)
