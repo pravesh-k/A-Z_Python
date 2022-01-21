59a9214d42bffb92baab8c25758b2f08d28db323
Pravesh <praveshnayak@gmail.com>
2022-01-21 23:45:23 +0530

    major: added more ways to use print in python3

--- 1_intro.py
+++ 1_intro.py
@@ -2,13 +2,16 @@
 # Beginning of Python3
 
 # Single line comments in python is identified-
-# by a hash at the beginning of the comment
+# by a hash at the beginning of the comment.
 
-# print to the console
+# Basic use case of inbuilt print function, this displays output on the console.
 print('First Hello World')
 print("Second Hello World")
 print('\n\n')
+
+# print(A)    #This produces error
 print('A', 'B', 'z')
+
 print(10)
 print(1, '\t', 2, '\t\t', 3)
 print('')
@@ -18,6 +21,21 @@ print('\n')
 print(10, 20, 30, sep='#')
 print(10, 20, 30, end='#')
 print()
+print(True)
+print(False)
+
+#complex number
+print(3+4j)     
+
+# print('Hello ' + 5 + ' World')     #This produces error
 print('A'+'b'+'X')
-# print('Hello ' + 5 + 'World')     #This produces error
+print('Hello ' + '5' + ' World')
+
+# str() is a global built-in function that converts an object into its string representation.
+print('Hello ' + str(6) + ' World')
+
+print('A '*5)
+
 # print(any_Object)
+
+# Some more use cases of print function with variables and different datatypes can be seen in further programs.
