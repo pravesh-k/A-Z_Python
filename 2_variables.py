1631b6272ec5f36fb55110d7a69ae96e97c3b0c7
Pravesh <praveshnayak@gmail.com>
2022-01-22 23:20:43 +0530

    initial commit: ways to define variables

---
+++ 2_variables.py
@@ -0,0 +1,22 @@
+# # Contents:
+#   1. Commenting
+#   2. The in-built print function
+
+# Defining variables in Python3
+
+a = 1
+print(a)
+b = 0
+print(b)
+c = True
+print(c)
+d = False
+print(d)
+e = 10
+print(e)
+f = 00
+print(f)
+# g = A     # Produces error
+print(g)
+h = 'A'
+print(h)
