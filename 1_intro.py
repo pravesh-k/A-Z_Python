5a3cc3e177da4aa49f229635e0d0aaefcafc91d3
Pravesh <praveshnayak@gmail.com>
2022-01-23 00:28:35 +0530

    major: indentation in python

--- 1_intro.py
+++ 1_intro.py
@@ -3,8 +3,10 @@
 
 # Contents:
 #   1. Commenting
-#   2. The in-built print function
+#   2. Indentation
+#   3. The in-built print function
 
+# 1 Commenting
 # Single line comments in python is identified-
 # by a hash at the beginning of the comment.
 
@@ -15,8 +17,27 @@ quote or double quotes can be
 used to serve the purpose
 of multi-line comment'''
 
+# 2 Indentation:
+# Python uses indentation to highlight the blocks of code. Whitespace is used 
+# for indentation in Python. All statements with the same distance to the right 
+# belong to the same block of code. If a block has to be more deeply nested, it 
+# is simply indented further to the right. You can understand it better by 
+# looking at the following lines of code.
+
+# Commented Python program showing indentation
+   
+# site = 'gfg'
+
+# if site == 'gfg': 
+#     print('Logging on to geeksforgeeks...') 
+# else: 
+#     print('retype the URL.') 
+# print('All set !')
+
+# 3 The in-built print function
 # Basic use case of inbuilt print function,
 # this displays output on the console.
+
 print('First Hello World')
 print("Second Hello World")
 print('\n\n')
