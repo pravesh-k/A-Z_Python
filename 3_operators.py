cfa0e6a0c8439c7902d10a59139091a3cde28704
Pravesh <praveshnayak@gmail.com>
2022-01-23 23:52:33 +0530

    initial commit: Arithmetic Operators

---
+++ 3_operators.py
@@ -0,0 +1,28 @@
+# # Contents:
+#   1. Arithmetic Operators
+#   2. Assignment operators
+#   3. Comparison operators
+#   4. Logical operators
+#   5 .Identity operators
+#   6. Membership operators
+#   7. Bitwise operators
+
+# 1. Arithmetic Operators: used with numeric values to perform common 
+# mathematical operations
+
+a = 10
+b = 5
+# Addition
+print(a + b)
+# Subtraction
+print(a - b)
+# Multiplication
+print(a * b)
+# Division
+print(a / b)
+# Modulus
+print(a % b)
+# Exponentiation
+print(a ** b)
+# Floor division
+print(a // b)
