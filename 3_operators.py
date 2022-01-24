a69e96f771a2fc63eec2a62fe5488d94f5308abd
Pravesh <praveshnayak@gmail.com>
2022-01-24 14:18:36 +0530

    major: identity, membership and bitwise operators

--- 3_operators.py
+++ 3_operators.py
@@ -113,3 +113,47 @@ print('8 < 5 or 8 < 10 returns', 8 < 5 or 8 < 10)
 # 'not' operator
 print('not(3 < 5 and 3 < 10) returns', not(3 < 5 and 3 < 10))
 print('not(8 < 5 and 8 < 10) returns', not(8 < 5 and 8 < 10), '\n')
+
+
+# 5. Identity Operators: used to compare the objects, not if they are equal, but 
+# if they are actually the same object, with the same memory location
+
+# 'is' operator
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print('x is z returns', x is z)
+print('x is y returns', x is y)     #even though x and y has same content, they 
+# are not in the same memory, whereas z is assigned the value of x that implies z 
+# will be accessing the value from memory location directly
+
+print('x == y returns', x == y, '\n')     #this illustrates that x and y has equal values
+
+
+# 6. Membership Operators: used to test if a sequence is presented in an object
+
+# 'in' operator
+print("'now' in 'acknowlwdgement' returns", 'now' in 'acknowlwdgement')
+print("'hello' in 'acknowlwdgement' returns", 'hello' in 'acknowlwdgement')
+
+# 'not in' operator
+print("'now' not in 'acknowlwdgement' returns", 'now' not in 'acknowlwdgement')
+print("'hello' not in 'acknowlwdgement' returns", 'hello' not in 'acknowlwdgement', '\n')
+
+
+# 7. Bitwise Operators: used to compare (binary) numbers
+
+x, y = 6, 2
+
+# AND: Sets each bit to 1 if both bits are 1
+print('x & y returns', x & y)
+# OR: Sets each bit to 1 if one of two bits is 1
+print('x | y returns', x | y)
+# XOR: Sets each bit to 1 if only one of two bits is 1
+print('x ^ y returns', x ^ y)
+# NOT: Inverts all the bits
+print('~y returns', y)
+# ZERO FILLED LEFT SHIFT: Shift left by pushing zeros in from the right and let the leftmost bits fall off
+print('x >> y returns', x >> y)
+# SIGNED RIGHT SHIFT: Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+print('x << y returns', x << y, '\n')
