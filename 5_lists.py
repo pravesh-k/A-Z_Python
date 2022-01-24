ce0b02c6158e4eab76111156d40b996d2f1b4723
Pravesh <praveshnayak@gmail.com>
2022-01-24 22:22:27 +0530

    major: accessing items of a list

--- 5_lists.py
+++ 5_lists.py
@@ -18,7 +18,7 @@ empty_list_1 = []
 empty_list_2 = list()
 
 # list with all items having type int
-prime_num_list = [5, 2, 3, 11, 7]
+prime_num_list = [5, 2, 3, 11, 7, 17, 13]
 magic_num_list = list((2, 8, 20, 28, 50))       #note the double parentheses
 
 # list with all items having type string
@@ -35,6 +35,7 @@ list_of_lists = [['banana', 'guava'], [True, False, True], [0, 15, 17, 55]]
 
 
 # 2. printing lists
+
 print('empty_list_1:\n', empty_list_1,
     '\nempty_list_2:\n', empty_list_2,
     '\nprime_num_list:\n', prime_num_list,
@@ -42,8 +43,51 @@ print('empty_list_1:\n', empty_list_1,
     '\nfruit_list:\n', fruit_list,
     '\nbool_list:\n', bool_list,
     '\nemp_det_list:\n', emp_det_list,
-    '\nlist_of_lists:\n', list_of_lists
+    '\nlist_of_lists:\n', list_of_lists,
+    '\n'
     )
 
 
 # 3. Accessing items from list
+
+# access single item
+print('fruit_list[0] =', fruit_list[0],
+    '\nprime_num_list[2] =', prime_num_list[2],
+    '\nlist_of_lists[0] =', list_of_lists[0],
+    '\nlist_of_lists[0][1] =', list_of_lists[0][1],
+    '\n'
+    )
+
+# access a sub sequence from the list/ this is 
+# also referred to as slicing a list
+print('fruit_list[0:1] =', fruit_list[0:2],
+    '\nprime_num_list[1:3] =', prime_num_list[1:3],
+    '\nlist_of_lists[1:2] =', list_of_lists[1:2],
+    '\n'
+    )
+
+# access sub sequence from a list but with 
+# a step/hop between items
+# template of slice operator: a_list[start:end:step]
+print('prime_num_list[0:4:2] =', prime_num_list[0:4:2],
+    '\nprime_num_list[0:6:2] =', prime_num_list[0:6:3],
+    '\n')
+
+# access sub sequence from a list but from end to start
+print('prime_num_list[4:0:-1] =', prime_num_list[4:0:-1],
+    '\nprime_num_list[6:0:-1] =', prime_num_list[6:0:-1],
+    '\nprime_num_list[6:0:-2] =', prime_num_list[6:0:-2],
+    '\n')
+
+# to access all items of list using slicing
+print('prime_num_list[:] =', prime_num_list[:],
+    '\nprime_num_list[::-1] =', prime_num_list[::-1],
+    '\n')
+
+# some more examples of slicing
+print('prime_num_list[:3] =', prime_num_list[:3],
+    '\nprime_num_list[3:] =', prime_num_list[3:],
+    '\nprime_num_list[-3:] =', prime_num_list[-3:],
+    '\nprime_num_list[:-3] =', prime_num_list[:-3],
+    '\nprime_num_list[::2] =', prime_num_list[::2],
+    '\n')
