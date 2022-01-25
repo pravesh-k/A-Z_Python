11b89ee6bccf60e12611fdbfe876672531cd2891
Pravesh <praveshnayak@gmail.com>
2022-01-25 12:46:21 +0530

    major: pop, clear, index and count methods of list

--- 5_lists.py
+++ 5_lists.py
@@ -94,12 +94,16 @@ print('prime_num_list[:3] =', prime_num_list[:3],
     )
 
 
+# ***************************************************
 # **note 1: inbuilt function id() can be used to find 
 #       the memory address of any variable in python3
 
+# example:
 print('memory address of fruit_list =', id(fruit_list),
     '\n'
     )
+# ***************************************************
+
 
 # 4. Copying a list to another variable
 
@@ -124,19 +128,23 @@ print('prime_num_list =', prime_num_list,
     )
 
 
+# ***************************************************
 # **note 2: inbuilt function len() can be used to return 
 #       the length (the number of items) of an object. 
 #       The argument may be a sequence (such as a string, 
 #       bytes, tuple, list, or range) or a collection 
 #       (such as a dictionary, set, or frozen set).
 
+# example:
 print('length of list fruit_list =', len(fruit_list),
     '\n')
+# ***************************************************
 
 
 # 5. List methods provided by Python3
 
 # a. Add and item to the end of the list
+#    syntax: a_list.append(object)
 prime_num_list.append(19)
 print('prime_num_list =', prime_num_list,
     '\n'
@@ -147,13 +155,17 @@ print('prime_num_list =', prime_num_list,
     '\n'
     )
 
+
+# ***************************************************
 # **note 3: Iterable is an object which can be looped over 
 #       or iterated over with the help of a for loop. Objects 
 #       like lists, tuples, sets, dictionaries, strings, etc. 
 #       are called iterables.
+# ***************************************************
 
 
 # b. Extend the list by appending all the items from the iterable
+#    syntax: a_list.extend(iterable_object)
 more_fruits_list = ['grapes', 'pineapple']
 fruit_list.extend(more_fruits_list)
 print('fruit_list =', fruit_list,
@@ -167,6 +179,7 @@ print('fruit_list =', fruit_list,
     '\n'
     )
 # c. Insert an item at a given position
+#    syntax: a_list.insert(index, object)
 more_fruits_list.insert(1, 'blackberry')
 print('more_fruits_list =', more_fruits_list,
     '\n'
@@ -177,3 +190,64 @@ more_fruits_list.insert(len(more_fruits_list), 'orange')
 print('more_fruits_list =', more_fruits_list,
     '\n'
     )
+
+# d. Remove the item at the given position in the list, and return it
+#    If no index is specified, removes and returns the item at the last
+#    syntax: a_list.pop(index_optional)
+print('more_fruits_list.pop(2) =', more_fruits_list.pop(2),
+    '\nmore_fruits_list =', more_fruits_list,
+    '\n'
+    )
+
+print('prime_num_list.pop() =', prime_num_list.pop(),
+    '\n')
+
+
+# ***************************************************
+# **note 4: 'del' keyword  in python is primarily used 
+#       to delete objects in Python. Since everything in 
+#       python represents an object in one way or another, 
+#       The del keyword can also be used to delete a list, 
+#       slice a list, delete a dictionaries, remove key-value 
+#       pairs from a dictionary, delete variables, etc.
+
+# example:
+x = 15
+print('x =', x)
+del x
+# print('x =', x)       # This raises a name error
+# ***************************************************
+
+
+# e. Remove all items from the list.
+#    syntax: a_list.clear()
+prime_num_list.clear()
+print('after clear() methods, prime_num_list =', prime_num_list,
+    '\n')
+
+# this is equivalent to the below operation
+del copy_prime_num_list[:]
+print('using the alternate way, copy_prime_num_list =', copy_prime_num_list,
+    '\n')
+
+
+# f. Return zero-based index in the list of the first 
+#    item whose value is equal to x. Raises a ValueError 
+#    if there is no such item.
+#    syntax: a_list.index(element, start, end) 
+#            where start and end parameters are optional
+print('fruit_list.index("cherry")', fruit_list.index('cherry'),
+    '\nfruit_list.index("pineapple", 3)', fruit_list.index('pineapple', 3),
+    '\nfruit_list.index("grapes", 1, 4)', fruit_list.index('grapes', 1, 4),
+    '\n')
+
+# g. Return the number of times an element/item appears in the list
+#    syntax: a_list.count(element)
+print('bool_list.count(True)', bool_list.count(True),
+    '\nbool_list.count(False)', bool_list.count(False),
+    '\nbool_list.count("Hello")', bool_list.count('Hello'),
+    '\nmore_fruits_list.count("blackberry")', more_fruits_list.count('blackberry'),
+    '\n'
+    )
+
+# h. 
