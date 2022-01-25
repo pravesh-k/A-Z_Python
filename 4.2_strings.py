113d17a2bb1253161f9199d213173dc91716c5f0
Pravesh <praveshnayak@gmail.com>
2022-01-26 02:00:39 +0530

    major: string slicing and escape sequences

--- 4.2_strings.py
+++ 4.2_strings.py
@@ -47,3 +47,40 @@ print(f'first character of string1 is: {string1[0]}'
     '\n')
 
 
+# 3. String Slicing
+
+# To access a range of characters in the String, the method of slicing is 
+# used. Slicing in a String is done by using a Slicing operator (colon).
+# Strings can't be accessed from backwards/reverse.
+
+print(f'slicing characters of string1 from 3-12: {string1[3:12]}'
+    f'\nslicing characters of string1 between 3rd and 2nd last character: {string1[3:-2]}'
+    f'\nslicing characters of string1 between 15th last and 2nd last character: {string1[-15:-2]}'
+    f'\nslicing characters of string1 after 10th character: {string1[10:]}'
+    f'\nslicing characters of string1 upto 10th character: {string1[:10]}'
+    '\n')
+
+
+# 4. Deleting/Updating in strings
+
+'''
+**note 2: Updation or deletion of characters from a String is not allowed. 
+This will cause an error because item assignment or item deletion from a 
+String is not supported. Although deletion of the entire String is 
+possible with the use of a built-in 'del' keyword. This is because Strings 
+are immutable, hence elements of a String cannot be changed once it has 
+been assigned. Only new strings can be reassigned to the same name. 
+'''
+
+statement = 'This is an old statement'
+print(f'statement: {statement}\n')
+# statement[2] = 'p'    # Raises TypeError
+statement = 'This is an updated statement'
+print(f'statement: {statement}\n')
+
+# del statement[3]      # Raises TypeError
+del statement
+# print(f'statement: {statement}\n')    # Raises NameError
+
+
+
