0a99e89e9b771be3e8b54a77bdd6ab75fa2da8a0
Pravesh <praveshnayak@gmail.com>
2022-01-27 23:13:40 +0530

    initial commit: tuples in python

---
+++ 6_tuples.py
@@ -0,0 +1,8 @@
+# Tuples in Python: A Tuple is a collection of Python objects separated by commas. 
+                # In someways a tuple is similar to a list in terms of indexing, 
+                # nested objects and repetition but a tuple is immutable unlike
+                # lists which are mutable.
+
+
+# # Contents:
+# 
