e17a49a4741d78d074beb6427ee1969e55b38a64
Pravesh <praveshnayak@gmail.com>
2022-01-30 23:49:40 +0530

    initial commit: decision making statements intro

---
+++ 9_if_else.py
@@ -0,0 +1,21 @@
+# Decision making statements: are those statements that are used to make some 
+#                             decisions and based on those decisions execution 
+#                             of next block of code is done
+
+# In Python, 'if' 'else' 'elif' statements are used for decision making. These 
+# statements control the flow of the logic and code.
+
+
+# # Contents:
+#   1. if statement
+#   2. else statement
+#   3. elif statement
+
+
+# 1. 'if' statement: 
+'''
+**note 1: if statement is the most simple decision-making statement. It is used to
+ decide whether a certain statement or block of statements will be executed or not 
+ i.e if a certain condition is true then a block of statement is executed 
+ otherwise not.
+'''
