62dade45d596c8cd49182bea2f268cfaa50b5984
Pravesh <praveshnayak@gmail.com>
2022-02-03 22:52:06 +0530

    major: arguments in function

--- 12_functions.py
+++ 12_functions.py
@@ -49,3 +49,28 @@ func()
 print(type(func), '\n')
 
 
+# 2. Arguments of a function
+'''Argument are the values passed inside the parenthesis of the function. A function can have any number of arguments separated by a comma.'''
+
+# Example 1: function with one argument
+def even_odd(num): 
+    print(f'{num} is even\n') if num % 2 == 0 else print(f'{num} is odd\n')
+
+even_odd(10)
+even_odd(17)
+
+# Example 2: function with three arguments
+def sum_of_3_num(num1, num3, num2):
+    print(f'sum = {num1+num2+num3}\n')
+
+sum_of_3_num(3, 4, 7)
+# sum_of_3_num(1, 2)    #TypeError: 3 arguments expected but provoded only 2
+
+'''
+**note 1:
+The terms parameter and argument can be used for the same thing: information that 
+are passed into a function. 
+From a function's perspective: A parameter is the variable listed inside the 
+parentheses in the function definition.
+An argument is the value that is sent to the function when it is called.
+'''
