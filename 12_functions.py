6b4c0fd82f9b181196725cded6b5727e2e2fd44c
Pravesh <praveshnayak@gmail.com>
2022-02-03 23:02:57 +0530

    major: default arguments in fuctions

--- 12_functions.py
+++ 12_functions.py
@@ -74,3 +74,47 @@ From a function's perspective: A parameter is the variable listed inside the
 parentheses in the function definition.
 An argument is the value that is sent to the function when it is called.
 '''
+
+# 3. Types of arguments
+
+# a. Default arguments: A default argument is a parameter that assumes a default 
+#                       value if a value is not provided in the function call for 
+#                       that argument.
+# Example 1:
+def my_func(x, y=50):
+    print(f'x is {x}')
+    print(f'y is {y}\n')
+
+my_func(10)
+my_func(10, 20)
+my_func(y = 10, x = 20)
+
+'''
+** note 2: 
+Any number of arguments in a function can have a default value. But once we have a default argument, all the arguments to its right must also have default values
+
+for ex:
+def func_2(a, b=10, c):     # SyntaxError: c can't not accessed
+    statement(s)
+'''
+
+def my_func_2(a, b=16, c=25):
+    print(f'a is {a}')
+    print(f'b is {b}')
+    print(f'c is {c}\n')
+
+my_func_2(9)
+my_func_2(9, 36)
+my_func_2(9, 64, 81)
+my_func_2(9, c=81)
+my_func_2(c=81, a=100)
+
+
+# 4. Docstring
+
+
+# 5. return statement
+
+
+# 6. Pass by Reference
+
