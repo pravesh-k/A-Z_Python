3b1125dae9fc89f46a13c67dfabbbb39b3e6b5e6
Pravesh <praveshnayak@gmail.com>
2022-02-04 23:58:04 +0530

    initial commit: lambda functions in python

---
+++ 13_lambda_recursion.py
@@ -0,0 +1,6 @@
+# Lambda Functions: Also known as Anonymous functions in python, are such 
+#                   functions which do not have a name. The lambda can take any 
+#                   number of arguments, but can only have one expression. While 
+#                   normal functions are defined using the 'def' keyword in 
+#                   Python, anonymous functions are defined using the 'lambda' 
+#                   keyword.
