c388647b58180b7369ebc11bc7cdc9a99a57350d
Pravesh <praveshnayak@gmail.com>
2022-02-07 23:56:17 +0530

    major: all() and ascii() functions

--- 14_inbuilt_functions.py
+++ 14_inbuilt_functions.py
@@ -5,12 +5,16 @@
 
 # 1. abs()
 '''
-This function returns the absolute value of the given number. If the number is a complex number, abs() returns its magnitude.
+This function returns the absolute value of the given number. If the number is 
+a complex number, abs() returns its magnitude.
 syntax:
 abs(num)
 where num can be integer, float and complex number
 '''
 # Example 1:
+from cv2 import norm
+
+
 num1 = -20
 num2 = -33.33
 c_num = 5+4j
@@ -22,7 +26,8 @@ print(f'absolute value of {num1} is {abs(num1)}'
 
 # 2. any()
 '''
-This function returns True if any element of an iterable is True. If not, it returns False.
+This function returns True if any element of an iterable is True. If not, it 
+returns False.
 syntax:
 any(iterable)
 '''
@@ -42,4 +47,41 @@ print(f'any(l1) where l1= {l1} returns {any(l1)}'
     f'\nany(l5) where l5= {l5} returns {any(l5)}'
     f'\nany(l6) where l6= {l6} returns {any(l6)}'
     f'\nany(l7) where l7= {l7} returns {any(l7)}'
+    '\n')
+
+
+# 3. all()
+'''
+This function returns True if all elements in the given iterable are true. If 
+not, it returns False.
+syntax:
+all(iterable)
+'''
+# Example 1: Using the variables from section 2
+
+print(f'all(l1) where l1= {l1} returns {all(l1)}'
+    f'\nall(l2) where l2= {l2} returns {all(l2)}'
+    f'\nall(l3) where l3= {l3} returns {all(l3)}'
+    f'\nall(l4) where l4= {l4} returns {all(l4)}'
+    f'\nall(l5) where l5= {l5} returns {all(l5)}'
+    f'\nall(l6) where l6= {l6} returns {all(l6)}'
+    f'\nall(l7) where l7= {l7} returns {all(l7)}'
+    '\n')
+
+
+# 4. ascii()
+# This function returns a string containing a printable representation of an 
+# object. It escapes the non-ASCII characters in the string using \x, \u or \U 
+# escapes.
+'''
+syntax:
+all(object)
+where the object can be string, list, etc
+'''
+# Example 1:
+normal_text = 'Python is interesting'
+other_text = 'Pythön is interesting'
+
+print(f'{normal_text} when passed to ascii() returns: {ascii(normal_text)}'
+    f'\n{other_text} when passed to ascii() returns: {ascii(other_text)}'
     '\n')
