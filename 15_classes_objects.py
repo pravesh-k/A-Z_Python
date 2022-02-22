de81dd8d485a2ff82459c1fe02c5335a209a3aff
Pravesh <praveshnayak@gmail.com>
2022-02-22 23:25:47 +0530

    initial commit: OOP, classes and objects in python

---
+++ 15_classes_objects.py
@@ -0,0 +1,19 @@
+'''
+Python is a multi-paradigm programming language which means that it supports different 
+approaches of programming. And OOPs is one of them.
+'''
+
+
+# OOPs: Object Oriented Programming. This pradigm of programming is based on "Objects".
+#       OOPs aims to implement real-world entities like inheritance, hiding, 
+#       polymorphism, etc in programming.
+
+# Object: In real world object is anything which is tangible and relatively stable. 
+#         Similarly in programming, objects are the interpretation of real world    
+#         objects and they posses some attributes and behaviour. Where, we refer 
+#         attributes as the data part of the object and behaviour as the methods 
+#         of the object.
+
+# Class: Collection of objects is called class. It is a logical entity. A class can 
+#        also be defined as a blueprint from which you can create an individual object. 
+#        Class doesn't consume any space.
