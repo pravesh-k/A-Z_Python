9714b1f6f1021cad790bfd770187fc6933488ffa
Pravesh <praveshnayak@gmail.com>
2022-02-23 23:53:01 +0530

    major: accessing and modifying value of attributes

--- 15_classes_objects.py
+++ 15_classes_objects.py
@@ -36,7 +36,7 @@ class Mammal:
     # this class has no attributes and no methods
 
 print(f'Passing Class name as an argument in print function output\'s\n{Mammal}\n'
-    f'\nAttributes of class Mammal:\n{dir(Mammal)}'
+    # f'\nAttributes of class Mammal:\n{dir(Mammal)}'
     '\n')
 
 # 2. Creating Objects (using class Mammal)
@@ -45,13 +45,13 @@ whale = Mammal()
 print(f'Passing Object name as an argument to print function, output\'s\n{whale}'
     f'\nClass type of whale: {type(whale)}'
     f'\nid of whale: {id(whale)}'
-    f'\n\nAttributes of whale: {dir(whale)}'
+    # f'\n\nAttributes of whale: {dir(whale)}'
     f'\n\nMammal class and whale object has same attributes?:\t{dir(Mammal) == dir(whale)}'
     '\n')
 
 
 # 3. Class Atrributes
-# # Example 2: Creating a class named Server with attributes
+# Example 1: Creating a class named Server with attributes
 class Server:
     no_vcpu = 4
     ram_capacity = 8
@@ -62,3 +62,15 @@ print(f'Attributes of Server Class:\n{dir(Server)}'
     f'\n\nMammal and Server class has same attributes?:\t{dir(Mammal) == dir(Server)}'
     f'\n\ndifference in attributes of Mammal and Server class?:\n{list(set(dir(Mammal)) - set(dir(Server))) + list(set(dir(Server)) - set(dir(Mammal)))}'
     '\n')
+
+# Example 2: Creating object from Server and accessing values of object attributes
+medium = Server()
+print(f'Mammal class and whale object has same attributes?:\t{dir(Server) == dir(medium)}'
+    f'\nvalue of no_vcpu attribute of medium object: {medium.no_vcpu}'
+    f'\nvalue of ram_capacity attribute of medium object: {medium.ram_capacity}'
+    f'\nvalue of storage attribute of medium object: {medium.storage}'
+    '\n')
+
+# Example 3: Changing value of attributes of object.
+medium.no_vcpu = 6
+print(f'value of no_vcpu attribute of medium object: {medium.no_vcpu}\n')
