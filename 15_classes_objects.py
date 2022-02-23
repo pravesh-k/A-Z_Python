0f0325f50a0f707250235ab2d04a1afa0e8b9ecc
Pravesh <praveshnayak@gmail.com>
2022-02-23 23:26:55 +0530

    major: creating class and object

--- 15_classes_objects.py
+++ 15_classes_objects.py
@@ -15,5 +15,32 @@ approaches of programming. And OOPs is one of them.
 #         of the object.
 
 # Class: Collection of objects is called class. It is a logical entity. A class can 
-#        also be defined as a blueprint from which you can create an individual object. 
+#        also be defined as a blueprint from which an individual object can be created. 
 #        Class doesn't consume any space.
+
+
+# # Contents:
+#   1. Creating Classes
+#   2. Creating Objects
+#   3. Class Attributes
+#   4. Class Methods
+#   5. Constructors
+
+
+# 1. Creating Classes
+# Classes can be created using the 'class' keyword.
+
+# Example 1: Creating a class to act as a blueprint for Mammals
+class Mammal:
+    pass
+    # this class has no attributes and no methods
+
+print(f'Passing Class name as an argument in print function output\'s\n{Mammal}\n')
+
+# 2. Creating Objects (using class Mammal)
+# Example 1: Creating an object named whale using the Mammal class
+whale = Mammal()
+print(f'Passing Object name as an argument to print function, output\'s\n{whale}'
+    f'\nClass type of whale: {type(whale)}'
+    f'\nid of whale: {id(whale)}'
+    '\n')
